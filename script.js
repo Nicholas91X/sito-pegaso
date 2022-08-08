7b76d32c4abf92e7657a5f06e3383757668b6b96
Nicholas <romitinicholas@gmail.com>
2022-08-08 20:58:52 +0200

    ultime modifiche

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function myFunction() {
   let element = document.getElementById("sub-menu");
+  let menu = document.getElementById("menu");
+  let menuX = document.getElementById("menu-x");
 
   if (element.style.display == "block") {
     element.style.display = "none";
+    menu.style.display = "block";
+    menuX.style.display = "none";
   } else {
     element.style.display = "block";
+    menu.style.display = "none";
+    menuX.style.display = "block";
   }
 }
