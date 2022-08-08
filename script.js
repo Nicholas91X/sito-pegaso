11911cc2a6f38317575795ee4433b8f882c1b4a1
Nicholas <romitinicholas@gmail.com>
2022-08-08 13:28:23 +0200

    nuovo header

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  let element = document.getElementById("sub-menu");
+
+  if (element.style.display == "block") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "block";
+  }
+}
